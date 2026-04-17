10eb4ac0ec8b50b4b585c8c43245e46fbbc0864b
BuildMesh <232736145+buildmesh@users.noreply.github.com>
2026-04-17 10:44:43 -0700

    launcher: Add ssm convenience script to connect to instances by name using SSM session

--- aws/launcher/init/deps.sh
+++ aws/launcher/init/deps.sh
@@ -21,3 +21,20 @@ apt-get install -y --no-install-recommends \
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
+# ------------------------------------------------------------
+# SSM convenience script
+# ------------------------------------------------------------
+log "Adding 'ssm' convenience script..."
+
+mkdir -p /home/ubuntu/.local/bin
+chown ubuntu.ubuntu /home/ubuntu/.local/bin
+echo 'PATH="$HOME/.local/bin:$PATH"' >> /home/ubuntu/.bashrc
+cat > "/home/ubuntu/.local/bin/ssm" << EOF
+#!/bin/sh
+
+instance_id=\$(aws ec2 describe-instances --filters "Name=tag:Name,Values=\$1" "Name=instance-state-name,Values=running" --query "Reservations[0].Instances[0].InstanceId" --output text)
+echo "Connecting to \$1 (\$instance_id)"
+aws ssm start-session --region us-west-2 --target "\$instance_id"
+EOF
+chown ubuntu.ubuntu /home/ubuntu/.local/bin/ssm
+chmod 500 /home/ubuntu/.local/bin/ssm
